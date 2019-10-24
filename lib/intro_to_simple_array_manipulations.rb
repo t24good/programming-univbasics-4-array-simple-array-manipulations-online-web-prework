e0c0f69cbf45e1bb1c35aafd7e3b430febfeec7c
Tanesha Goodman <t24good@icloud.com>
2019-10-24 18:52:12 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(colors_in_the_rainbow,next_color)
+colors_in_the_rainbow = ["blue", "green", "cheese"]
+next_color= "violet"
+colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  bouroughs_in_nyc = ["hood","state", "police", "bronx"]
+  new_neighborhood = "Staten Island"
+bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(continents)
+  continents = ["Africa", "Asia","Europe","North America","South America","Oceania","Antarctica"]
+  continents.pop
+end
