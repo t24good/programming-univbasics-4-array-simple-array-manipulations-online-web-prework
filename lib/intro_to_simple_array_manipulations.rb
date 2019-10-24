4c5f6d4dae82ab5474ee624e153122ec46d21958
Tanesha Goodman <t24good@icloud.com>
2019-10-24 18:54:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,6 +11,5 @@ bouroughs_in_nyc.unshift(new_neighborhood)
 end
 
 def using_pop(continents)
-  continents = ["Africa", "Asia","Europe","North America","South America","Oceania","Antarctica"]
   continents.pop
 end
