40e31f300fbe3dc6b1a467b41de079c1ef6bfe81
Tanesha Goodman <t24good@icloud.com>
2019-10-24 21:07:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,6 +31,6 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert (list_of_programming_languages, element)
- list_of_programming_languages.insert(element)
+ list_of_programming_languages.insert(4,element)
  
 end
